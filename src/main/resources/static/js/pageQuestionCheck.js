a0425e5017064f562ceab6443c5ea9bd04945c00
tammy61 <minmin199361@qq.com>
2015-07-30 20:25:14 +0800

    1、修改了一下新题审阅

--- src/main/resources/static/js/pageQuestionCheck.js
+++ src/main/resources/static/js/pageQuestionCheck.js
@@ -26,6 +26,7 @@ $(function(){
 //	    }
 //	});
 //	
+	//分页
 	$("#page").page({
 		showInfo: true,
 	    //showJump: true,
@@ -34,7 +35,7 @@ $(function(){
 	    //total:1000
 	    remote: {
 	        url: '/getJsonData',  //请求地址
-	        params: { query: "test" },       //自定义请求参数
+	        params: { subject: "小学语文" },       //自定义请求参数
 	        beforeSend: function(XMLHttpRequest){
 	            //...
 	        },
@@ -52,4 +53,92 @@ $(function(){
 	        totalName: 'total'            //指定返回数据的总数据量的字段名
 	    }
 	});
+	
+	//是否展开完整信息
+	$(document).on("click",'input[name="checkboxForFullInfo"]',function(){
+		//alert('绑定成功');
+		var div=$(this).parent().parent().parent();
+		if($(this).prop('checked')){
+			$(div).find('div[name="divFullInfo"]').css("display",""); 
+		}else{
+			$(div).find('div[name="divFullInfo"]').css("display","none"); 
+		}
+	});
+	
+	//单题审核通过
+	$(document).on("click",'button[name="btnPassForOneQuestion"]',function(){
+		//alert('btnPassForOneQuestion绑定成功');
+		//$(this).removeClass();
+		$(this).parent().find('button').addClass('disabled');
+		var parents = $(this).parentsUntil('div[name="divQuestionBox"]');
+		var divparent = $(parents[parents.length-1]).parent();
+		//alert(divparent);
+		var id=$(divparent).find('input[name="questionId"]').val();
+		alert('pass question id:'+id);
+	});
+	
+	//单题修改
+	$(document).on("click",'button[name="btnModifyOneQuestion"]',function(){
+		//alert('btnPassForOneQuestion绑定成功');
+		//$(this).removeClass();
+		$(this).parent().find('button').addClass('disabled');
+		var parents = $(this).parentsUntil('div[name="divQuestionBox"]');
+		var divparent = $(parents[parents.length-1]).parent();
+		//alert(divparent);
+		var id=$(divparent).find('input[name="questionId"]').val();
+		alert('modify question id:'+id);
+	});
+	
+	//单题修改
+	$(document).on("click",'button[name="btnDelOneQuestion"]',function(){
+		//alert('btnPassForOneQuestion绑定成功');
+		//$(this).removeClass();
+		$(this).parent().find('button').addClass('disabled');
+		var parents = $(this).parentsUntil('div[name="divQuestionBox"]');
+		var divparent = $(parents[parents.length-1]).parent();
+		//alert(divparent);
+		var id=$(divparent).find('input[name="questionId"]').val();
+		alert('del question id:'+id);
+	});
+	
+	//多题审核通过
+	$('button[name="btnPassQuestions"]').click(function(){
+		var idArr=[];
+		$('input[name="checkBoxForSelect"]:checked').each(function(){
+			var parents=$(this).parentsUntil('div[name="divQuestionBox"]');
+			var divparent = $(parents[parents.length-1]).parent();
+			//alert(divparent);
+			var id=$(divparent).find('input[name="questionId"]').val();
+			idArr.push(id);
+		});
+		
+		alert(JSON.stringify(idArr));
+	});
+	
+	//全选
+	$('button[name="btnSelectAllQuestions"]').click(function(){
+		$('input[name="checkBoxForSelect"]').each(function(){
+			$(this).prop('checked',true);
+		});
+	});
+	
+	//多题删除
+	$('button[name="btnDelQuestions"]').click(function(){
+		var idArr=[];
+		$('input[name="checkBoxForSelect"]:checked').each(function(){
+			var parents=$(this).parentsUntil('div[name="divQuestionBox"]');
+			var divparent = $(parents[parents.length-1]).parent();
+			//alert(divparent);
+			var id=$(divparent).find('input[name="questionId"]').val();
+			idArr.push(id);
+		});
+		
+		alert(JSON.stringify(idArr));
+	});
+	
+	//选择科目
+	$(document).on("click",'a[name="subjectSubName2"]'),function(){
+		
+	});
+	
 });
