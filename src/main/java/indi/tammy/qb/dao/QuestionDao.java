d8bb13c5f25deaab602fcd5073ebb36aa6deb739
min_g <675980887@qq.com>
2015-07-31 17:16:14 +0800

    增加题库查询语句，返回查询结果

--- src/main/java/indi/tammy/qb/dao/QuestionDao.java
+++ src/main/java/indi/tammy/qb/dao/QuestionDao.java
@@ -1,5 +1,7 @@
 package indi.tammy.qb.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
 import indi.tammy.qb.model.Question;
@@ -12,4 +14,7 @@ public interface QuestionDao {
 	public void update(Question question);
 	
 	public void findByParam(@Param(value="know")int know, @Param(value="area")int area, @Param(value="standard")int standard, @Param(value="grade")int grade);
+
+	public List<Question> findBySubject(@Param(value="subject") String subject, @Param(value="pStart") int pStart, @Param(value="pEnd") int pEnd);
+
 }
--- src/main/java/indi/tammy/qb/service/QuestionService.java
+++ src/main/java/indi/tammy/qb/service/QuestionService.java
@@ -1,5 +1,7 @@
 package indi.tammy.qb.service;
 
+import java.util.List;
+
 import indi.tammy.qb.dao.QuestionDao;
 import indi.tammy.qb.model.Question;
 
@@ -17,5 +19,9 @@ public class QuestionService {
 	
 	public void delete(int id){
 		questionDao.delete(id);
+	}
+	
+	public List<Question> findBySubject(String subject, int pStart, int pEnd){
+		return questionDao.findBySubject(subject, pStart, pEnd);
 	}
 }
