01f865ec746291ab54b2f440b3228cbc04c5b2a6
min_g <675980887@qq.com>
2015-08-10 17:22:26 +0800

    文件上传返回错误

---
+++ src/main/java/indi/tammy/qb/controller/WordBankController.java
@@ -0,0 +1,44 @@
+package indi.tammy.qb.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class WordBankController {
+	
+	
+	/**
+	 * 手动导入单词页面
+	 * @return
+	 */
+	@RequestMapping(value={"/admin/lexicon/manualImport"},method = RequestMethod.GET)
+	public String pageManualImport(){
+		return "pagesLexicon/pagesWordsImport/pageManualImport";
+	}
+	
+	/**
+	 * word导入单词页面
+	 * @return
+	 */
+	@RequestMapping(value={"/admin/lexicon/wordImport"},method = RequestMethod.GET)
+	public String pageWordImport(){
+		return "pagesLexicon/pagesWordsImport/pageWordImport";
+	}
+	
+	@RequestMapping(value={"/admin/lexicon/wordImport/uploader"},method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<String> wordImportUploader(@RequestParam("file") MultipartFile upfile){
+		return new ResponseEntity<String>("123", HttpStatus.BAD_REQUEST);
+	}
+	
+	
+}
--- src/main/resources/static/js/pagesLexicon/pageWordImport.js
+++ src/main/resources/static/js/pagesLexicon/pageWordImport.js
@@ -7,7 +7,7 @@ $(function(){
 	    swf:'/plugins/webUploader/Uploader.swf',
 
 	    // 文件接收服务端。
-	    server: '/upload/file',
+	    server: '/admin/lexicon/wordImport/uploader',
 
 	    // 选择文件的按钮。可选。
 	    // 内部根据当前运行是创建，可能是input元素，也可能是flash.
