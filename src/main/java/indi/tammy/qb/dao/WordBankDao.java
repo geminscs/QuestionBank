18d3891ef471849059866f9ad59b535e8b85a515
min_g <675980887@qq.com>
2015-08-12 21:57:46 +0800

    实现8-12

--- src/main/java/indi/tammy/qb/dao/WordBankDao.java
+++ src/main/java/indi/tammy/qb/dao/WordBankDao.java
@@ -13,9 +13,13 @@ public interface WordBankDao {
 	
 	public void delete(int id);
 	
+	public void formalDelete(int id);
+	
 	public void update(WordBank w);
 	
 	public List<WordBank> findByType(@Param(value="pStart")int pStart, @Param(value="pEnd")int pEnd, @Param(value="type")int type);
 
 	public WordBank findById(int id);
+	
+	public List<WordBank> formalFindByParam(@Param(value="pStart")int pStart, @Param(value="pEnd")int pEnd, @Param(value="type")int type, @Param(value="grade")int grade, @Param(value="key")String key);
 }
